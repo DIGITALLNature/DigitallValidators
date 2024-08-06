9fe58fe6cfc034114bebe029ab28998a4656b243
Lars Bauer <lars.bauer@digitall.com>
2024-08-06 11:02:55 +0200

    initial commit

---
+++ src/dgt.solutions.Validators/PhoneNumber/PhoneNumberErrorCodes.cs
@@ -0,0 +1,16 @@
+﻿namespace dgt.solutions.Validators.PhoneNumber
+{
+  public static class PhoneNumberErrorCodes
+  {
+    // PhoneNumbers.ErrorType
+    public const string InvalidCountryCode = "INVALID_COUNTRY_CODE";
+    public const string NotANumber = "NOT_A_NUMBER";
+    public const string TooShortAfterIdd = "TOO_SHORT_AFTER_IDD";
+    public const string TooShortNsn = "TOO_SHORT_NSN";
+    public const string TooLong = "TOO_LONG";
+    
+    // custom error codes
+    public const string RegionMismatch = "REGION_MISMATCH";
+    public const string InvalidPhoneNumber = "INVALID_PHONE_NUMBER";
+  }
+}
---
+++ src/dgt.solutions.Validators/PhoneNumber/ValidatePhoneNumber.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Globalization;
+using Digitall.APower;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using PhoneNumbers;
+
+namespace dgt.solutions.Validators.PhoneNumber
+{
+  public class ValidatePhoneNumber : IPlugin
+  {
+    public void Execute(IServiceProvider serviceProvider)
+    {
+      var executionContext = serviceProvider.Get<IPluginExecutionContext>(); // serviceProvider.GetExecutionContext();
+      var inputParameters = ParseInputParameters(executionContext);
+
+      var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+      // parse
+      PhoneNumbers.PhoneNumber phoneNumber;
+      try
+      {
+        phoneNumber = phoneNumberUtil.Parse(inputParameters.PhoneNumber, inputParameters.Region);
+      }
+      catch (NumberParseException e)
+      {
+        executionContext.SetOutputParameter("IsValid", false);
+        executionContext.SetOutputParameter("ErrorCode", e.ErrorType.ToString());
+        executionContext.SetOutputParameter("Message", e.Message);
+        return;
+      }
+
+      // validate
+      var isValid = string.IsNullOrWhiteSpace(inputParameters.Region)
+        ? ValidateWithoutRegion(phoneNumberUtil, phoneNumber, executionContext)
+        : ValidateWithRegion(phoneNumberUtil, phoneNumber, executionContext, inputParameters.Region);
+      if (!isValid) return;
+
+      // format
+      var formattedPhoneNumber = phoneNumberUtil.Format(phoneNumber, inputParameters.Format);
+      executionContext.SetOutputParameter("FormattedPhoneNumber", formattedPhoneNumber);
+    }
+
+    private static bool ValidateWithRegion(PhoneNumberUtil phoneNumberUtil, PhoneNumbers.PhoneNumber phoneNumber,
+      IPluginExecutionContext executionContext, string region)
+    {
+      var isValid = phoneNumberUtil.IsValidNumberForRegion(phoneNumber, region);
+      executionContext.SetOutputParameter("IsValid", isValid);
+      if (!isValid)
+      {
+        executionContext.SetOutputParameter("ErrorCode", PhoneNumberErrorCodes.RegionMismatch);
+        executionContext.SetOutputParameter("Message", $"Phone number is not valid for region '{region}'.");
+      }
+
+      return isValid;
+    }
+
+    private static bool ValidateWithoutRegion(PhoneNumberUtil phoneNumberUtil, PhoneNumbers.PhoneNumber phoneNumber,
+      IPluginExecutionContext executionContext)
+    {
+      var isValid = phoneNumberUtil.IsValidNumber(phoneNumber);
+      executionContext.SetOutputParameter("IsValid", isValid);
+      if (!isValid)
+      {
+        executionContext.SetOutputParameter("ErrorCode", PhoneNumberErrorCodes.InvalidPhoneNumber);
+        executionContext.SetOutputParameter("Message", "Phone number is not valid.");
+      }
+
+      return isValid;
+    }
+
+    private static PhoneNumberValidationParameters ParseInputParameters(IPluginExecutionContext executionContext)
+    {
+      executionContext.GetInputParameter("PhoneNumber", out string phoneNumber);
+      executionContext.GetInputParameter("Region", out string region);
+      executionContext.GetInputParameter("Format", out string format);
+
+      var regionCode = ParseRegionCode(region);
+      var phoneNumberFormat = ParsePhoneNumberFormat(format);
+
+      return new PhoneNumberValidationParameters
+      {
+        PhoneNumber = phoneNumber, Region = regionCode, Format = phoneNumberFormat
+      };
+    }
+
+    private static string ParseRegionCode(string region)
+    {
+      if (string.IsNullOrWhiteSpace(region)) return null;
+
+      try
+      {
+        var regionInfo = new RegionInfo(region);
+        return regionInfo.TwoLetterISORegionName;
+      }
+      catch (ArgumentException)
+      {
+        throw new InvalidPluginExecutionException(OperationStatus.Succeeded,
+          $"'{region}' is not a valid ISO 3166 two-letter country code.");
+      }
+    }
+
+    private static PhoneNumberFormat ParsePhoneNumberFormat(string format)
+    {
+      if (string.IsNullOrWhiteSpace(format)) return PhoneNumberFormat.E164;
+
+      if (!Enum.TryParse(format.ToUpperInvariant(), out PhoneNumberFormat phoneNumberFormat))
+        throw new InvalidPluginExecutionException(OperationStatus.Succeeded,
+          $"Format '{format}' is not valid. Possible values are: {string.Join(",", Enum.GetNames(typeof(PhoneNumberFormat)))}");
+
+      return phoneNumberFormat;
+    }
+
+    private class PhoneNumberValidationParameters
+    {
+      public string PhoneNumber { get; set; }
+      public string Region { get; set; }
+      public PhoneNumberFormat Format { get; set; }
+    }
+  }
+}
